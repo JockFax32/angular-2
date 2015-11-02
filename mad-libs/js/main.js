47ef65a5b94570ce2bd087c1a714b026454ff998
JockFax32 <foxtjack@gmail.com>
2015-11-02 13:48:24 -0800

    Mad Libs

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,7 +1,15 @@
 // Create application
-
+var madApp = angular.module("madApp",[])
 
 // Bind controller madCtrl
+madApp.controller("mainCtrl",function($scope){
+    $scope.name
+    $scope.age
+    $scope.location
+    $scope.food
+
+
+});
 
 
 	// Set initial values for name, age, location, and food
